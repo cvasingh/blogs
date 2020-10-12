e48f00c8ec026c1f85b6fd003d5810be99e4a274
Shiva Singh <71915162+cvasingh@users.noreply.github.com>
2020-10-12 21:02:21 +0530

    Add files via upload

---
+++ common.php
@@ -0,0 +1,4 @@
+<?php
+$hostname ="http://localhost/8point7mminus01/";
+$conn = mysqli_connect("localhost", "root","","8point7mminus01") or die("connection failed: ".mysqli_connect_error()) ;
+?>
---
+++ footer.php
@@ -0,0 +1,19 @@
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <center>
+                    <h4>
+                    © Copyright 2020 <a href="">8.7-1</a> . All Rights Reserved  | Follow Us On: 
+                    <a href="" class="fa fa-facebook-official"></a>
+                    <a href="" class="fa fa-instagram"></a>
+                    <a href="" class="fa fa-twitter"></a>
+                    </h4>
+                </center>
+               
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
